7de2e7d7b5ef1eb480bb38155c483bd335ffa146
Dongkwan Shin <scalcor@gmail.com>
2023-02-20 22:53:08 +0900

    linear gradient

--- result_folding.js
+++ result_folding.js
@@ -1,4 +1,5 @@
 function on_expand() {
     const expanded = document.getElementById('content-1').className === 'cont-content-expanded';
     document.getElementById('content-1').className = expanded ? 'cont-content-collapsed' : 'cont-content-expanded';
+    document.getElementById('dim-1').style.visibility = expanded ? "hidden" : "";
 }
